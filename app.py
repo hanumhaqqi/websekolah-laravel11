a72576b32567b69b41895b27686609fa87407908
hanumhaqqi <hanumhaqqi88@gmail.com>
2024-09-10 09:23:51 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,9 @@
+def luas_segitiga():
+    a = int(input("Masukan alas segitiga :"))
+    t = int(input("Masukan tinggi Segitiga :"))
+
+    luas = a * t / 2
+    print("Luas segitiga adalah :", luas)
+luas_segitiga()
+
+    
